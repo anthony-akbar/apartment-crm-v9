b0b85961741b1afb6317ed1a26b18816bfe5ab27
mamadjanovakbar@gmail.com <AkbarGithub8522>
2024-02-22 12:46:04 +0600

    Clients error fix

--- resources/views/clients/table.blade.php
+++ resources/views/clients/table.blade.php
@@ -37,7 +37,7 @@
                 </td>
                 <td class="table-report__action">
                     <div class="flex justify-center items-center">
-                        <a class="flex items-center text-primary whitespace-nowrap mr-5" href="javascript:;">
+                        <a class="flex items-center text-primary whitespace-nowrap mr-5" href="{{ route('clients.edit', $client->id) }}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                  stroke-linejoin="round" icon-name="check-square" data-lucide="check-square"
@@ -45,7 +45,7 @@
                                 <polyline points="9 11 12 14 22 4"></polyline>
                                 <path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"></path>
                             </svg>
-                            Детали </a>
+                            Изменить </a>
                         <a class="flex items-center text-primary whitespace-nowrap" href="javascript:;"
                            data-tw-toggle="modal" data-tw-target="#delete-confirmation-modal-{{ $client->id }}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
@@ -97,11 +97,11 @@
 <!-- BEGIN: Pagination -->
 <div class="intro-y col-span-12 flex flex-wrap sm:flex-row sm:flex-nowrap items-center">
 
-    <div class="grid grid-cols-2">
+    {{--<div class="grid grid-cols-2">
         <div class="mt-10 grid-cols-1 text-center">
             {{$clients->links()}}
         </div>
-    </div>
+    </div>--}}
 
     {{--<nav class="w-full sm:w-auto sm:mr-auto">
         <ul class="pagination">
